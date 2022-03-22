6fe67f9c0cf8c908674d9b4ff5b98aae8d3501f6
prathapkammeta <prathapkammeta@gmail.com>
2022-03-22 18:39:27 +0530

    1 file modified, 3 added

--- src/App.js
+++ src/App.js
@@ -1,11 +1,25 @@
-import React from "react";
-import "./style.css";
+import React from 'react';
+import './style.css';
 
-export default function App() {
+import SearchTodos from './TodoList';
+import TodoList from './TodoList';
+import SearchUsers from './UsersList';
+import UsersList from './UsersList';
+
+function App() {
   return (
-    <div>
-      <h1>Hello StackBlitz!</h1>
-      <p>Start editing to see some magic happen :)</p>
+    <div className="App">
+      <h2>Higher Order Component</h2>
+      <div className="section">
+        <div>
+          <SearchUsers />
+        </div>
+        <div>
+          <SearchTodos />
+        </div>
+      </div>
     </div>
   );
 }
+
+export default App;
